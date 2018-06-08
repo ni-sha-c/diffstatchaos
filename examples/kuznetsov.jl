d59a0b5696daa001f7d9b0ff596ac267b6aa5a88
Nisha Chandramoorthy <nishac92@gmail.com>
2018-06-07 21:43:46 -0400

    Add adjoint

--- examples/kuznetsov.jl
+++ examples/kuznetsov.jl
@@ -1,6 +1,7 @@
 dt = 1.e-2
 s0 = [1.0,1.0]
 d = 3
+p = 2
 boundaries = ones(2*d)
 boundaries[1] = -1.0
 boundaries[2] = -1.0
@@ -25,16 +26,9 @@ function Step(u0::Array{Float64,1},s::Array{Float64,1},n::Int64,
 		r = sqrt(r2)
 		
 		t = (t0 + (i-1)*dt)%T
-			
-		if(t >= 1.0 && t < 2.0)
-			σ = -1.0
-		elseif(t >= 4.0 && t < 5.0)
-			σ = 1.0
-		elseif(t >= 2.0 && t < 3.0)
-			a = -1.0
-		elseif(t >= 5.0 && t < 6.0)
-			a = 1.0
-		end	
+		σ = diff_rot_freq(t)
+		a = rot_freq(t)		
+	
 		coeff1 = σ*pi*0.5*(z*sqrt(2) + 1)
 		coeff2 = s[1]*(1. - σ*σ - a*a)
 		coeff3 = s[2]*a*a*(1.0 - r)		
@@ -81,4 +75,158 @@ function stereographic_projection(u::Array{Float64,1})
 
 end
 
+function tangent_step(v0::Array{Float64,1},u::Array{Float64,1},
+					 s::Array{Float64,1},
+					 ds::Array{Float64,1},
+					 t::Float64)
 
+	x = u[1]
+	y = u[2]
+	z = u[3]
+
+	dx = v0[1]
+	dy = v0[2]
+	dz = v0[3]
+	v = copy(v0)
+
+	σ = 0.0
+	a = 0.0	
+	r2 = x^2 + y^2 + z^2	
+	r = sqrt(r2)
+		
+	t = t%T
+	σ = diff_rot_freq(t)
+	a = rot_freq(t)
+
+	
+	coeff1 = σ*pi*0.5*(z*sqrt(2) + 1)
+	coeff2 = s[1]*(1. - σ*σ - a*a)
+	coeff3 = s[2]*a*a*(1.0 - r)		
+
+	dcoeff1_dz = σ*pi*0.5*sqrt(2)
+	dcoeff2_ds1 = coeff2/s[1]
+	dcoeff3_ds2 = coeff3/s[2]
+	dcoeff3_dx = s[2]*a*a*(-x)/r
+	dcoeff3_dy = s[2]*a*a*(-y)/r
+	dcoeff3_dz = s[2]*a*a*(-z)/r
+		
+
+
+	v[1] += dt*(-1.0*dcoeff1_dz*y*dz - 1.0*
+				coeff1*dy - dcoeff2_ds1*ds[1]*x*y*y - 
+				coeff2*y*y*dx - coeff2*x*2.0*y*dy + 
+				0.5*a*pi*dz + dcoeff3_ds2*ds[2]*x + 
+				dcoeff3_dx*x*dx + 
+				dcoeff3_dy*x*dy + 
+				dcoeff3_dz*x*dz +
+				coeff3*dx)  
+
+	v[2] += dt*(coeff1*0.5*dx + 
+				dcoeff1_dz*0.5*x*dz + 
+				dcoeff2_ds1*y*x*x*ds[1] + 
+				coeff2*dy*x*x + 
+				coeff2*2.0*x*dx*y + 
+				dcoeff3_ds2*ds[2]*y + 
+				dcoeff3_dx*y*dx + 
+				dcoeff3_dy*y*dy +
+		 		dcoeff3_dz*y*dz +
+			 	coeff3*dy) 
+
+	v[3] += dt*(-0.5*a*pi*dx + 
+				dcoeff3_ds2*z*ds[2] + 
+				dcoeff3_dx*z*dx + 
+				dcoeff3_dy*z*dy + 
+				dcoeff3_dz*z*dz + 
+				coeff3*dz)	 
+
+	return v
+
+
+
+end
+
+function rot_freq(t::Float64)
+
+	if(t >= 2.0 && t < 3.0)
+		return -1.0
+	end
+
+	if(t >= 5.0 && t < 6.0)
+		return 1.0
+	end
+	
+	return 0.0
+
+end
+
+function diff_rot_freq(t::Float64)
+
+	if(t >= 1.0 && t < 2.0)
+		return -1.0
+	end
+
+	if(t >= 4.0 && t < 5.0)
+		return 1.0
+	end
+
+	return 0.0
+
+
+end
+
+function adjoint_step(y1::Array{Float64,1},u::Array{Float64,1},
+					 s::Array{Float64,1},
+					 dJ::Array{Float64,1},t::Float64)
+
+
+	y0 = copy(y1)
+
+	x = u[1]
+	y = u[2]
+	z = u[3]
+	#t = u[4]
+	
+	r2 = x^2 + y^2
+	r = sqrt(r2)
+	σ = diff_rot_freq(t)
+	a = rot_freq(t)		
+	coeff1 = σ*pi*0.5*(z*sqrt(2) + 1)
+	coeff2 = s[1]*(1. - σ*σ - a*a)
+	coeff3 = s[2]*a*a*(1.0 - r)		
+
+	dcoeff1dz = σ*pi*0.5*sqrt(2)
+	dcoeff3dx = s[2]*a*a*(-x)/r 
+	dcoeff3dy = s[2]*a*a*(-y)/r
+	dcoeff3dz = s[2]*a*a*(-z)/r 
+
+	y0[1] += y1[1]*dt*(-1.0*coeff2*y*y) + 
+			y1[1]*dt*coeff3 + 
+			y1[1]*dt*x*dcoeff3dx + 
+			y1[2]*dt*coeff1*0.5 + 
+			y1[2]*dt*y*2.0*x + 
+			y1[2]*dt*dcoeff3dx*y + 
+			y1[3]*dt*(-0.5)*a*pi + 
+			y1[3]*dt*z*dcoeff3dx
+
+
+	y0[2] += y1[1]*dt*(-1.0)*coeff1 - 
+			y1[1]*dt*(-1.0)*coeff2*x*2.0*y + 
+			y1[1]*dt*dcoeff3dy*x + 
+			y1[2]*dt*coeff2*x*x + 
+			y1[2]*dt*coeff3 + 
+			y1[2]*dt*dcoeff3dy*y + 
+			y1[3]*dt*dcoeff3dy*z
+
+	
+	y0[3] += y1[1]*dt*(-1.0)*dcoeff1dz*y + 
+			y1[1]*dt*0.5*a*pi + 
+			y1[1]*dt*dcoeff3dz*x + 
+			y1[2]*dt*dcoeff1dz*0.5*x + 
+			y1[2]*dt*y*dcoeff3dz + 
+			y1[3]*dt*z*dcoeff3dz + 
+			y1[3]*dt*coeff3
+		
+
+	return y0
+
+end	
--- examples/tests/test_step.jl
+++ examples/tests/test_step.jl
@@ -1,12 +1,12 @@
 testName="kuznetsov"
 include("../$testName.jl")
 using PyPlot
-#s0 = [0.72, 1.9]
-#function plot_attractor()
+
+function plot_attractor()
 
 	n_testpoints = 5000
 	n_times = 6
-	n_steps = n_poincare
+	n_stepsi = n_poincare
 	u0 = rand(d,n_testpoints)
 	u0 .*= (boundaries[d+1:2*d]-boundaries[1:d])
 	u0 .+= boundaries[1:d]
@@ -16,10 +16,10 @@ using PyPlot
 	subplot(331)
 	plot(u0[1,:],u0[2,:],"o")
 	for i = 1:n_testpoints
-		u[:,i,1] = Step(u[:,i,1],s0,100*n_steps,t)
+		u[:,i,1] = Step(u[:,i,1],s0,100*n_stepsi,t)
 		t = 100*n_poincare*dt
 		for n = 2:n_times
-			u[:,i,n] = Step(u[:,i,n-1],s0,100*n_steps,t)
+			u[:,i,n] = Step(u[:,i,n-1],s0,100*n_stepsi,t)
 			t += 100*n_poincare*dt
 		end
 		t = 0.0
@@ -58,4 +58,64 @@ using PyPlot
 	plot(x,y,"ro")
 
 
+end
+
+function test_tangent()
+
+	n_testpoints = 100
+	n_epsi = 8
+	
+	u0 = rand(d,n_testpoints)
+	epsi = logspace(-n_eps,-1.0,n_epsi)
+	vu_fd = zeros(d,n_testpoints,n_epsi)
+	vs_fd = zeros(d,n_testpoints,n_epsi)
+	vu_ana = zeros(d,n_testpoints)
+	vs_ana = zeros(d,n_testpoints)
+	u0next = zeros(d)
+	v0 = rand(3)
+	ds0 = [0.,1.]
+	for i = 1:n_testpoints
+		u0[:,i] = Step(u0[:,i],s0,10*n_poincare,0.0)	
+		t = 6.0*rand()
+		for k = 1:n_epsi		
+			u0next = Step(u0[:,i],s0,1,t)
+			vu_fd[:,i,k] = (Step(u0[:,i] + epsi[k]*v0,
+						 s0,1,t)-u0next)/epsi[k]
+			
+			vs_fd[:,i,k] = (Step(u0[:,i],s0 + epsi[k]*ds0,1,
+							t) - u0next)/epsi[k]
+
+		end
+
+		vu_ana[:,i] = tangent_step(v0,u0[:,i],s0,zeros(p),t)
+		vs_ana[:,i] = tangent_step(zeros(d),u0[:,i],s0,ds0,t)
+
+	
+	end		
+
+	erru = zeros(n_epsi)
+	errs = zeros(n_epsi)
+
+	[erru[k] = vecnorm(vu_ana[:,:]-vu_fd[:,:,k]) for k = 1:n_epsi]
+	[errs[k] = vecnorm(vs_ana[:,:]-vs_fd[:,:,k]) for k = 1:n_epsi]
+
+	figure()
+	loglog(epsi,erru)
+	figure()
+	loglog(epsi,errs)
+
+
+
+end
+
+#function test_testadjoint()
+	u = rand(3)
+	t = rand()
+	y1 = rand(3)
+	v0 = rand(3)
+	v1 = tangent_step(v0,u,s0,zeros(3),t)
+	y0 = adjoint_step(y1,u,s0,zeros(3),t)
+	println(dot(v1,y1))
+	println(dot(v0,y0))	
 #end
+
